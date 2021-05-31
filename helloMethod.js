a45c73274332d4255edadc5e692b8a8873c2d9a7
fireballdev99 <fireball.dev99@gmail.com>
2021-05-31 20:58:21 +0700

    initial commit

---
+++ helloMethod.js
@@ -0,0 +1,3 @@
+module.exports = function myHello() {
+    console.log('Hello');
+}
---
+++ main.js
@@ -0,0 +1,9 @@
+const myHello = require('./helloMethod')
+
+class main {
+    myHello = myHello
+}
+
+const myMain = new main()
+
+myMain.myHello()
