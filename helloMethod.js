d7316d2053c214cd89cbedca664a077c68495420
fireballdev99 <fireball.dev99@gmail.com>
2021-05-31 23:12:39 +0700

    nano id

--- helloMethod.js
+++
@@ -1,3 +0,0 @@
-module.exports = function myHello() {
-    console.log('Hello');
-}
--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-const myHello = require('./helloMethod')
+//โจทย์ก็คือ ให้ทำการเขียน lib / nanoid โดยการสุ่ม ตัวอักษร สามารถรับตัวแปร คำนำหน้าได้ และกรณีไม่มีคำนำหน้า ให้เป็น SA_ ดังเดิม
+//รวมถึงสามารถแสดงความยาวของการสุ่มได้ด้วยเช่นกัน โดยฟังก์ชันสุ่มมีสองแบบ 1.สุ่มตัวเลข และความยาว 2.สุ่มตัวอักษรและความยาว
+const Nanoid = require('./lib/nanoid')
 
-class main {
-    myHello = myHello
+// console.log(new Nanoid(20, 'SA').genID());
+
+const genID = (length, tag) => {
+    length = length
+    tag = tag
+    const gen = new Nanoid(length, tag).genID()
+    return (
+        console.log(gen)
+    )
 }
 
-const myMain = new main()
+const genNumID = (length, tag) => {
+    length = length
+    tag = tag
+    const gen = new Nanoid(length, tag).genNumberID()
+    return (
+        console.log(gen)
+    )
+}
 
-myMain.myHello()
+genID(20, 'SA')
+genNumID(10, 'BE')
